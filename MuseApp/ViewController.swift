9a20708e76d8c0d6baed7b7c62411532208fa024
Anton Veldanov <anton.veldanov@gmail.com>
2021-12-09 12:41:30 -0800

    first commit

---
+++ MuseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MuseApp
+//
+//  Created by Anton Veldanov on 12/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
