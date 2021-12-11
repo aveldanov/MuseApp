3b6f3653a51d467bab5bb0d47f90edd5da7712b0
Anton Veldanov <anton.veldanov@gmail.com>
2021-12-10 17:01:35 -0800

    iOS

--- MuseApp/Controllers/Core Tabs/ProfileViewController.swift
+++ MuseApp/Controllers/Core Tabs/ProfileViewController.swift
@@ -12,18 +12,16 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Sign Out",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapSignOut))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func didTapSignOut(){
+        
     }
-    */
+ 
 
 }
