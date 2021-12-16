65f0187b4cafd64ebc505bd0bd9bae53afe18914
Anton Veldanov <anton.veldanov@gmail.com>
2021-12-15 23:55:27 -0800

    iOS

--- MuseApp/Controllers/Auth/SignInViewController.swift
+++ MuseApp/Controllers/Auth/SignInViewController.swift
@@ -9,19 +9,24 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    // Header View
+    
+    // Email Field
+    
+    // password field
+    
+    // Sign in button
+    
+    // Create accoutn button
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Sign In"
         view.backgroundColor = .systemBackground
                 
-        DispatchQueue.main.asyncAfter(deadline: .now()+3) {
-            if !IAPManager.shared.isPremium(){
-                let vc = PayWallViewController()
-                let navVC = UINavigationController(rootViewController: vc)
-                self.present(navVC, animated: true, completion: nil)
-            }
-        }
+
     }
     
 
---
+++ MuseApp/Views/SignInHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInHeaderView.swift
+//  MuseApp
+//
+//  Created by Anton Veldanov on 12/13/21.
+//
+
+import UIKit
+
+class SignInHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
