a00283e38621fb0535aae2183d4beb28298f664e
Anton Veldanov <anton.veldanov@gmail.com>
2021-12-25 16:58:07 -0800

    iOS

--- MuseApp/Controllers/Core Tabs/ProfileViewController.swift
+++ MuseApp/Controllers/Core Tabs/ProfileViewController.swift
@@ -20,6 +20,19 @@ class ProfileViewController: UIViewController {
     }
     
     @objc private func didTapSignOut(){
+        AuthManager.shared.signOut { [weak self] success in
+            if success{
+                DispatchQueue.main.async {
+                    let signinVC = SignInViewController()
+                    signinVC.navigationItem.largeTitleDisplayMode = .always
+                    
+                    let navVC = UINavigationController(rootViewController: signinVC)
+                    navVC.navigationBar.prefersLargeTitles = true
+                    navVC.modalPresentationStyle = .fullScreen
+                    self?.present(navVC, animated: true, completion: nil)
+                }
+            }
+        }
         
     }
  
