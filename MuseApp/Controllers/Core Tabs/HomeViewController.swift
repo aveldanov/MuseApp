79b434dc94d9d64d43b6aecbe9d85f1b0d850e0b
Anton Veldanov <anton.veldanov@gmail.com>
2021-12-29 15:00:14 -0800

    iOS

--- MuseApp/Controllers/Core Tabs/HomeViewController.swift
+++ MuseApp/Controllers/Core Tabs/HomeViewController.swift
@@ -14,20 +14,28 @@ class HomeViewController: UIViewController {
         let button = UIButton()
         button.backgroundColor  = .systemBlue
         button.tintColor = .white
-        button.setImage(UIImage(systemName: "pencil"), for: .normal)
+        button.setImage(UIImage(systemName: "square.and.pencil", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium)), for: .normal)
         button.layer.cornerRadius = 40
         button.layer.shadowColor = UIColor.label.cgColor
         button.layer.shadowOpacity = 0.4
-        
+        button.layer.shadowRadius = 10
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-
+        view.addSubview(composeButton)
     }
 
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        composeButton.frame = CGRect(x: view.frame.width-80-16,
+                                     y: view.frame.height-80-16-view.safeAreaInsets.bottom,
+                                     width: 80,
+                                     height: 80)
+    }
     
     
 
