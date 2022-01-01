431b8814dfbf074e212fed7e5e16c4fefe1b8bfa
Anton Veldanov <anton.veldanov@gmail.com>
2022-01-01 00:10:35 -0800

    iOS

--- MuseApp/Controllers/Posts/ViewPostViewController.swift
+++ MuseApp/Controllers/Posts/ViewPostViewController.swift
@@ -33,12 +33,14 @@ class ViewPostViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+        tableView.frame = view.bounds
     }
 
     
@@ -47,4 +49,41 @@ class ViewPostViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+
+extension ViewPostViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let index = indexPath.row
+        // 0: title
+        // 1: image
+        // 2: text
+        
+        switch index{
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = post.title
+            return cell
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: PostHeaderTableViewCell.identifier, for: indexPath) as? PostHeaderTableViewCell else{
+                fatalError()
+            }
+            cell.configureCell(with: .init(imageURL: post.headerImageURL))
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = post.text
+            return cell
+        default:
+            fatalError()
+        }
+        
+        return
+        
+    }
+    
 }
--- MuseApp/Views/Cells/PostHeaderTableViewCell.swift
+++ MuseApp/Views/Cells/PostHeaderTableViewCell.swift
@@ -7,19 +7,72 @@
 
 import UIKit
 
+class PostHeaderTableViewCellViewModel{
+    let imageURL: URL?
+    var imageData: Data?
+    
+    init(imageURL: URL?){
+        self.imageURL = imageURL
+    }
+    
+}
+
+
+
 class PostHeaderTableViewCell: UITableViewCell {
 
     static let identifier = "PostHeaderTableViewCell"
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+    private let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.clipsToBounds = true
+        contentView.addSubview(postImageView)
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        postImageView.frame = contentView.bounds
+    }
+    
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        postImageView.image = nil
+        
+    }
+    
+    func configureCell(with viewModel: PostHeaderTableViewCellViewModel){
+        
+        if let data = viewModel.imageData{
+            postImageView.image = UIImage(data: data)
+        }else if let url = viewModel.imageURL{
+            //Fetch image and cache it
+            URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
+                guard let data = data, error == nil else {
+                    return
+                }
+                //caching as part of VM
+                viewModel.imageData = data
+                DispatchQueue.main.async {
+                    self?.postImageView.image = UIImage(data: data)
+                }
+            }.resume()
+        }
+    }
+    
 
 }
